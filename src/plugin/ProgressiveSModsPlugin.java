afa44ef57156b9808265ac1d411aad2f859cc39b
Alex Chen <hy_pi_corism@live.com>
2021-11-16 15:31:16 -0500

    Rename mod and mod plugin

--- src/plugin/ProgressiveSModsPlugin.java
+++ src/plugin/ProgressiveSModsPlugin.java
@@ -7,7 +7,7 @@ import campaign.EngagementResultListener;
 import util.SModUtils;
 import util.SModUtils.ShipDataTable;
 
-public class RevampedSModsPlugin extends com.fs.starfarer.api.BaseModPlugin {
+public class ProgressiveSModsPlugin extends com.fs.starfarer.api.BaseModPlugin {
 
     @Override
     public void onGameLoad(boolean newGame) {
