8df8bb2a20715e350a96565855ee80b845d11867
Alex Chen <hy_pi_corism@live.com>
2021-11-20 22:05:45 -0500

    Tabs to spaces

--- src/data/campaign/rulecmd/ProgSModSetStoryOption.java
+++ src/data/campaign/rulecmd/ProgSModSetStoryOption.java
@@ -17,70 +17,70 @@ import com.fs.starfarer.api.util.Misc.Token;
  *  Uses default sound. Modified from SetStoryOption in the base game. */
 public class ProgSModSetStoryOption extends SetStoryOption {
 
-	private class ProgSModStoryPointActionDelegate extends BaseOptionStoryPointActionDelegate {
-		ProgSModStoryPointActionDelegate(InteractionDialogAPI dialog, String optionId, int cost, int bonusPercent) {
-			super(dialog, new StoryOptionParams(optionId, cost, "", "general", null));
-			bonusXPFraction = (float) bonusPercent / 100f;
-		}
+    private class ProgSModStoryPointActionDelegate extends BaseOptionStoryPointActionDelegate {
+        ProgSModStoryPointActionDelegate(InteractionDialogAPI dialog, String optionId, int cost, int bonusPercent) {
+            super(dialog, new StoryOptionParams(optionId, cost, "", "general", null));
+            bonusXPFraction = (float) bonusPercent / 100f;
+        }
 
-		String getOptionId() {
-			return (String) optionId;
-		}
+        String getOptionId() {
+            return (String) optionId;
+        }
 
-		int getCost() {
-			return numPoints;
-		}
+        int getCost() {
+            return numPoints;
+        }
 
-		String getSoundId() {
-			return soundId;
-		}
-	}
+        String getSoundId() {
+            return soundId;
+        }
+    }
 
-	@Override
-	public boolean execute(String ruleId, final InteractionDialogAPI dialog, List<Token> params, Map<String, MemoryAPI> memoryMap) {
-		if (dialog == null) return false;
+    @Override
+    public boolean execute(String ruleId, final InteractionDialogAPI dialog, List<Token> params, Map<String, MemoryAPI> memoryMap) {
+        if (dialog == null) return false;
 
-		String optionId = params.get(0).string;
-		int numPoints = params.get(1).getInt(memoryMap);
-		int bonusXPPercent = params.get(2).getInt(memoryMap);
+        String optionId = params.get(0).string;
+        int numPoints = params.get(1).getInt(memoryMap);
+        int bonusXPPercent = params.get(2).getInt(memoryMap);
 
-		return set(dialog, numPoints, optionId, bonusXPPercent);
-	}
+        return set(dialog, numPoints, optionId, bonusXPPercent);
+    }
 
-	private boolean set(InteractionDialogAPI dialog, int numPoints, String optionId, int bonusXPPercent) {
-			return set(dialog, new ProgSModStoryPointActionDelegate(dialog, optionId, numPoints, bonusXPPercent));
-	}
+    private boolean set(InteractionDialogAPI dialog, int numPoints, String optionId, int bonusXPPercent) {
+            return set(dialog, new ProgSModStoryPointActionDelegate(dialog, optionId, numPoints, bonusXPPercent));
+    }
 
-	private ProgSModStoryPointActionDelegate delegate = null;
+    private ProgSModStoryPointActionDelegate delegate = null;
 
-	private boolean set(InteractionDialogAPI dialog,
-			ProgSModStoryPointActionDelegate del) {
-		delegate = del;
-		int cost = delegate.getCost();
-		String optionId = delegate.getOptionId();
-		float bonusXPFraction = delegate.getBonusXPFraction();
-		dialog.makeStoryOption(optionId, cost, bonusXPFraction, delegate.getSoundId());
-		
-		if (cost > Global.getSector().getPlayerStats().getStoryPoints()) {
-			dialog.getOptionPanel().setEnabled(optionId, false);
-		}
-		
-		dialog.getOptionPanel().addOptionTooltipAppender(optionId, new OptionTooltipCreator() {
-			public void createTooltip(TooltipMakerAPI tooltip, boolean hadOtherText) {
-				float opad = 10f;
-				float initPad = 0f;
-				if (hadOtherText) initPad = opad;
-				tooltip.addStoryPointUseInfo(initPad, delegate.getCost(), delegate.getBonusXPFraction(), true);
-				int sp = Global.getSector().getPlayerStats().getStoryPoints();
-				String points = "points";
-				if (sp == 1) points = "point";
-				tooltip.addPara("You have %s " + Misc.STORY + " " + points + ".", opad, 
-						Misc.getStoryOptionColor(), "" + sp);
-			}
-		});
-		
-		dialog.getOptionPanel().addOptionConfirmation(optionId, delegate);
-		
-		return true;
-	}
+    private boolean set(InteractionDialogAPI dialog,
+            ProgSModStoryPointActionDelegate del) {
+        delegate = del;
+        int cost = delegate.getCost();
+        String optionId = delegate.getOptionId();
+        float bonusXPFraction = delegate.getBonusXPFraction();
+        dialog.makeStoryOption(optionId, cost, bonusXPFraction, delegate.getSoundId());
+        
+        if (cost > Global.getSector().getPlayerStats().getStoryPoints()) {
+            dialog.getOptionPanel().setEnabled(optionId, false);
+        }
+        
+        dialog.getOptionPanel().addOptionTooltipAppender(optionId, new OptionTooltipCreator() {
+            public void createTooltip(TooltipMakerAPI tooltip, boolean hadOtherText) {
+                float opad = 10f;
+                float initPad = 0f;
+                if (hadOtherText) initPad = opad;
+                tooltip.addStoryPointUseInfo(initPad, delegate.getCost(), delegate.getBonusXPFraction(), true);
+                int sp = Global.getSector().getPlayerStats().getStoryPoints();
+                String points = "points";
+                if (sp == 1) points = "point";
+                tooltip.addPara("You have %s " + Misc.STORY + " " + points + ".", opad, 
+                        Misc.getStoryOptionColor(), "" + sp);
+            }
+        });
+        
+        dialog.getOptionPanel().addOptionConfirmation(optionId, delegate);
+        
+        return true;
+    }
 }
--- src/data/campaign/rulecmd/util/ProgSModBuildInPlugin.java
+++ src/data/campaign/rulecmd/util/ProgSModBuildInPlugin.java
@@ -17,42 +17,42 @@ import data.campaign.rulecmd.util.ProgSModSelectPanelCreator.SelectorData;
 import util.SModUtils;
 public class ProgSModBuildInPlugin implements CustomUIPanelPlugin {
 
-	private LabelAPI nSelectedLabel, remainingXPLabel;
-	private List<SelectorData> selectorList;
-	private FleetMemberAPI fleetMember;
-	private float shipXP;
-	private int numCanBuildIn;
+    private LabelAPI nSelectedLabel, remainingXPLabel;
+    private List<SelectorData> selectorList;
+    private FleetMemberAPI fleetMember;
+    private float shipXP;
+    private int numCanBuildIn;
 
-	public void setData(LabelAPI nSelected, LabelAPI remainingXP, List<SelectorData> list, FleetMemberAPI member) {
-		nSelectedLabel = nSelected;
-		remainingXPLabel = remainingXP;
-		selectorList = list;
-		fleetMember = member;
-		shipXP = SModUtils.getXP(fleetMember.getId());
-		numCanBuildIn = SModUtils.getMaxSMods(fleetMember) - fleetMember.getVariant().getSMods().size();
+    public void setData(LabelAPI nSelected, LabelAPI remainingXP, List<SelectorData> list, FleetMemberAPI member) {
+        nSelectedLabel = nSelected;
+        remainingXPLabel = remainingXP;
+        selectorList = list;
+        fleetMember = member;
+        shipXP = SModUtils.getXP(fleetMember.getId());
+        numCanBuildIn = SModUtils.getMaxSMods(fleetMember) - fleetMember.getVariant().getSMods().size();
 
-		if (!SModUtils.Constants.IGNORE_NO_BUILD_IN) {
-			removeCantBuildIn();
-		}
-	}
+        if (!SModUtils.Constants.IGNORE_NO_BUILD_IN) {
+            removeCantBuildIn();
+        }
+    }
 
-	/** Remove the hull mods that can't be built in (i.e. SO)
-	 *  from the button list and disable them. */
-	private void removeCantBuildIn() {
-		Iterator<SelectorData> itr = selectorList.iterator();
-		while (itr.hasNext()) {
-			SelectorData entry = itr.next();
-			HullModSpecAPI hullMod = Global.getSettings().getHullModSpec(entry.hullModId);
-			if (hullMod.hasTag("no_build_in")) {
-				ProgSModSelectPanelCreator.disableEntryRed(entry);
-				entry.costLabel.setText("Cannot be built in");
-				entry.costLabel.setHighlight("Cannot be built in");
-				itr.remove();
-			}
-		}
-	}
+    /** Remove the hull mods that can't be built in (i.e. SO)
+     *  from the button list and disable them. */
+    private void removeCantBuildIn() {
+        Iterator<SelectorData> itr = selectorList.iterator();
+        while (itr.hasNext()) {
+            SelectorData entry = itr.next();
+            HullModSpecAPI hullMod = Global.getSettings().getHullModSpec(entry.hullModId);
+            if (hullMod.hasTag("no_build_in")) {
+                ProgSModSelectPanelCreator.disableEntryRed(entry);
+                entry.costLabel.setText("Cannot be built in");
+                entry.costLabel.setHighlight("Cannot be built in");
+                itr.remove();
+            }
+        }
+    }
 
-	/** Returns the pair (number of checked buttons, sum of checked buttons' XP costs). 
+    /** Returns the pair (number of checked buttons, sum of checked buttons' XP costs). 
      * Populates [checkedEntries] with the checked buttons if a non-null list was passed in.*/
     public Pair<Integer, Integer> tallyCheckedEntries(List<SelectorData> selectorList, List<SelectorData> checkedEntries) {
         if (!checkedEntries.isEmpty()) {
@@ -69,65 +69,65 @@ public class ProgSModBuildInPlugin implements CustomUIPanelPlugin {
         return new Pair<>(numChecked, sumChecked);
     }
 
-	@Override
-	public void positionChanged(PositionAPI position) {}
-	
-	@Override
-	public void advance(float amount) {}
+    @Override
+    public void positionChanged(PositionAPI position) {}
+    
+    @Override
+    public void advance(float amount) {}
 
-	@Override
-	public void processInput(List<InputEventAPI> events) {
-		if (selectorList == null) {
-			return;
-		}
+    @Override
+    public void processInput(List<InputEventAPI> events) {
+        if (selectorList == null) {
+            return;
+        }
 
-		for (InputEventAPI event : events) {
-			if (event.isConsumed()) {
-				continue;
-			}
+        for (InputEventAPI event : events) {
+            if (event.isConsumed()) {
+                continue;
+            }
 
-			// Ideally this would be a mouse down event, or even better,
-			// a callback from a button being clicked, but the first
-			// get consumed by the button click and the second doesn't seem to
-			// exist.
-			if (event.isMouseMoveEvent()) {
-				Pair<Integer, Integer> numAndSum = tallyCheckedEntries(selectorList, new ArrayList<SelectorData>());
-				float remainingXP = shipXP - numAndSum.two;
+            // Ideally this would be a mouse down event, or even better,
+            // a callback from a button being clicked, but the first
+            // get consumed by the button click and the second doesn't seem to
+            // exist.
+            if (event.isMouseMoveEvent()) {
+                Pair<Integer, Integer> numAndSum = tallyCheckedEntries(selectorList, new ArrayList<SelectorData>());
+                float remainingXP = shipXP - numAndSum.two;
 
-				// Update the xp remaining and number selected labels
-				ProgSModSelectPanelCreator.setNSelectedModsText(nSelectedLabel, numAndSum.one, numCanBuildIn);
-				ProgSModSelectPanelCreator.setRemainingXPText(remainingXPLabel, remainingXP);
+                // Update the xp remaining and number selected labels
+                ProgSModSelectPanelCreator.setNSelectedModsText(nSelectedLabel, numAndSum.one, numCanBuildIn);
+                ProgSModSelectPanelCreator.setRemainingXPText(remainingXPLabel, remainingXP);
 
-				// Disable or enable buttons depending on the remaining XP
-				// and hull mod build-in slots
-				for (SelectorData entry : selectorList) {
-					if (entry.button.isChecked()) {
-						continue;
-					}
-					if (entry.hullModCost > remainingXP) {
-						if (entry.button.isEnabled()) {
-							ProgSModSelectPanelCreator.disableEntryRed(entry);
-						}
-					}
-					else if (numAndSum.one >= numCanBuildIn) {
-						if (entry.button.isEnabled()) {
-							ProgSModSelectPanelCreator.disableEntryGray(entry);
-						}
-					}
-					else {
-						if (!entry.button.isEnabled()) {
-							ProgSModSelectPanelCreator.enableEntry(entry);
-						}
-					}	
-				}
-			}
-		}
-	}
+                // Disable or enable buttons depending on the remaining XP
+                // and hull mod build-in slots
+                for (SelectorData entry : selectorList) {
+                    if (entry.button.isChecked()) {
+                        continue;
+                    }
+                    if (entry.hullModCost > remainingXP) {
+                        if (entry.button.isEnabled()) {
+                            ProgSModSelectPanelCreator.disableEntryRed(entry);
+                        }
+                    }
+                    else if (numAndSum.one >= numCanBuildIn) {
+                        if (entry.button.isEnabled()) {
+                            ProgSModSelectPanelCreator.disableEntryGray(entry);
+                        }
+                    }
+                    else {
+                        if (!entry.button.isEnabled()) {
+                            ProgSModSelectPanelCreator.enableEntry(entry);
+                        }
+                    }	
+                }
+            }
+        }
+    }
 
-	@Override
-	public void render(float alphaMult) {
-	}
+    @Override
+    public void render(float alphaMult) {
+    }
 
-	@Override
-	public void renderBelow(float alphaMult) {}
+    @Override
+    public void renderBelow(float alphaMult) {}
 }
